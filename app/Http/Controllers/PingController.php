7c842d68bc886bd4e02a8d3cbd356327bbedc838
hncg <574878649@qq.com>
2015-12-07 17:09:27 +0800

    hello cg

--- app/Http/Controllers/PingController.php
+++ app/Http/Controllers/PingController.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+use Input;
 
 class PingController extends Controller{
 
     public function ping()
     {
-        return '1';
+        return 'hello cg';
     }
 }
