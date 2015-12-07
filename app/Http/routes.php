d7dcb94f04f4208057175df8ab85e17aab1e5e8e
hncg <574878649@qq.com>
2015-12-07 11:17:25 +0800

    路由群组

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,3 +1,8 @@
 <?php
 
 Route::get('/ping', 'PingController@ping');
+Route::group(['prefix' => '/v1'], function(){
+    Route::group(['prefix' => '/user/{user_id}'], function(){
+        Route::get('/profile', 'UserController@profile');
+    });
+});
