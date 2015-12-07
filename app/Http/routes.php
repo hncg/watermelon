346522a0b306f78c17b8b67867341f121b923538
hncg <574878649@qq.com>
2015-12-07 17:17:53 +0800

    增加where字段规则过滤

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,7 +2,7 @@
 
 Route::get('/ping', 'PingController@ping');
 Route::group(['prefix' => '/v1'], function(){
-    Route::group(['prefix' => '/user/{user_id}'], function(){
-        Route::get('/profile', 'UserController@profile');
+    Route::group(['prefix' => '/user/{user_id}', 'where' => ['user_id' => '[0-9]+'],'middleware' => 'cg'], function(){
+        Route::post('/profile', 'UserController@profile');
     });
 });
