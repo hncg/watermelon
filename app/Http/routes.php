e54e92bd768a8a704ed39d177a343c75ef898d6c
hncg <574878649@qq.com>
2016-02-21 15:07:39 +0800

    add Apicontroller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 Route::get('/ping', 'PingController@ping');
+Route::get('/api', 'ApiController@api');
 Route::group(['prefix' => '/v1'], function(){
     Route::group(['prefix' => '/user/{user_id}', 'where' => ['user_id' => '[0-9]+'],'middleware' => 'cg'], function(){
         Route::get('/profile', 'UserController@profile');
