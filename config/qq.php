e29e70530d826668f76be66617e96dd00bfb747a
hncg <574878649@qq.com>
2016-03-05 22:04:19 +0800

    修改call back地址

--- config/qq.php
+++ config/qq.php
@@ -1,4 +1,4 @@
 <?php
-return json_decode('{"appid":"101293593","appkey":"de948be84d76470c3730084b10c7ba0f","callback":"http://blog.cg.com/api/v1/login_by_3_callback","scope":"get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}')
+return json_decode('{"appid":"101293593","appkey":"de948be84d76470c3730084b10c7ba0f","callback":"http://blog.che-cg.com/api/v1/login_by_3_callback","scope":"get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}')
 
 ?>
